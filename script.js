3dc77cacb8cc42f070689a44042d05f7b1a0aa86
552020 <stefanolombardo@posteo.de>
2021-05-16 20:31:27 +0200

    lecture 73 - starting some logic and own changes css

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number! 🎈';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 17;
+// document.querySelector('.score').textContent = 20;
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    // if the fiel is empty we get a 0 and 0 is a falsy value!
+    document.querySelector('.message').textContent = 'No number! 😑';
+  }
+});
