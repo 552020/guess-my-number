6e3e952f87a9228317415c72ecc6fb124793fe10
552020 <stefanolombardo@posteo.de>
2021-05-17 10:31:26 +0200

    shortening the query selector through variables my solution

--- script.js
+++ script.js
@@ -5,25 +5,29 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1; //  Math.floor(Math.rando
 let score = 20;
 let highscore = 0;
 
+let messageDom = document.querySelector('.message');
+let scoreDom = document.querySelector('.score');
+let numberDom = document.querySelector('.number');
+
 const play = function () {
   const guess = Number(document.querySelector('.guess').value);
 
   //*** if there is no input
   if (!guess) {
     // if the field is empty we get a 0 and 0 is a falsy value!
-    document.querySelector('.message').textContent = 'No number! 😑';
-    document.querySelector('.message').style.color = 'red';
-    document.querySelector('.message').style.backgroundColor = 'yellow';
+    messageDom.textContent = 'No number! 😑';
+    messageDom.style.color = 'red';
+    messageDom.style.backgroundColor = 'yellow';
     document.querySelector('body').style.backgroundColor = 'pink';
 
     // If player wins
   } else if (guess === secretNumber) {
-    score = document.querySelector('.score').textContent;
+    score = scoreDom.textContent;
 
-    document.querySelector('.message').textContent = 'Yes! 🎉';
+    messageDom.textContent = 'Yes! 🎉';
     document.querySelector('body').style.backgroundColor = 'red';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
+    numberDom.style.width = '30rem';
+    numberDom.textContent = secretNumber;
     // highscore: if version - a ternary version in the dirty file
     if (score > highscore) {
       highscore = score;
@@ -31,14 +35,14 @@ const play = function () {
     }
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
+      messageDom.textContent =
         guess > secretNumber ? 'Too high! 🚀' : 'Too low! 🕸';
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreDom.textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You loose! 😱';
+      messageDom.textContent = 'You loose! 😱';
 
-      document.querySelector('.score').textContent = 0;
+      scoreDom.textContent = 0;
     }
   }
 };
@@ -50,13 +54,13 @@ document.querySelector('.guess').addEventListener('keyup', function (event) {
 
 const reset = function () {
   score = 20;
-  document.querySelector('.score').textContent = score;
+  scoreDom.textContent = score;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.number').textContent = '?';
+  messageDom.textContent = 'Start guessing...';
+  numberDom.textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  numberDom.style.width = '15rem';
 };
 
 document.querySelector('.again').addEventListener('click', reset);
